2209cd3a781e0529857f5316350e7e105cec3010
Elvis-code <elviskovaci22@gmail.com>
2021-06-21 13:34:10 +0200

    Changes made on intelycare.js file and add  Video recording for all the tests

--- test/specs/intelycare.js
+++ test/specs/intelycare.js
@@ -4,7 +4,7 @@ const LoginForm = require('../pageobjects/login.form');
 describe('User can create account', () => {
     it('using valid credentials', async () => {
         await LoginPage.open();   //The page that allows the new user to create an account 
-        await LoginPage.login('user96@mailinator.com', 'User123%');
+        await LoginPage.login('useri1@mailinator.com', 'User123%');
     });
     
     
@@ -13,7 +13,7 @@ describe('User can create account', () => {
 describe('The Form on the basic screen info ', () => {          // The basic info screen that opens after the new user register the email and password
    
     it('should be filled correctly', async () => {
-        await LoginForm.fillForm('Username','Lastname','7589577506','02090'); // The user fill the fields with required information about personal information and get the Continue clickable
+        await LoginForm.fillForm('Firstname','Lastname','7589577506','02090'); // The user fill the fields with required information about personal information and get the Continue clickable
         browser.pause(10000);
         await expect(LoginForm.phonescreen).toBeExisting();
         await  expect(LoginForm.phonescreen).toHaveTextContaining(
