0cef2c07d98aae349ea1acba6d4d8771b3c25c83
Elvis-code <elviskovaci22@gmail.com>
2021-06-22 14:15:41 +0200

    Changes on intycare.js added button to be enabled

--- test/specs/intelycare.js
+++ test/specs/intelycare.js
@@ -13,7 +13,9 @@ describe('User can create account', () => {
 describe('The Form on the basic screen info ', () => {          // The basic info screen that opens after the new user register the email and password
    
     it('should be filled correctly', async () => {
+        await expect(LoginForm.contButton).toBeEnabled();
         await LoginForm.fillForm('Firstname','Lastname','7589577506','02090'); // The user fill the fields with required information about personal information and get the Continue clickable
+        
         browser.pause(10000);
         await expect(LoginForm.phonescreen).toBeExisting();
         await  expect(LoginForm.phonescreen).toHaveTextContaining(
